0e34008c253ff28829a4e9f179a1cfcdd8e49d8c
Victor Gomes Sampaio <victorg.sampaio@outlook.com>
2018-09-27 17:17:25 -0300

    Implementação da função alocaMatriz

--- codigo.c
+++ codigo.c
@@ -3,48 +3,66 @@
 #include <string.h>
 #include <pthread.h>
 
-int configuraMatriz(char nome_arq, int t){
+/* Funções do programa */
+float **alocarMatriz(int linhas, int colunas);
+int carrega(float **matriz, char nome_arq[]);
+int localiza(float **matriz, int valor);
+int imprimeSaida(float **matriz, int valor);
 
-    int m, n, v;
-
-    printf("\nDigite o numero de linhas da matriz: ");
-    scanf("%d", &m);
-
-    printf("\nDigite o numero de colunas da matriz: ");
-    scanf("%d", &n);
-
-    printf("\nDigite o valor a ser buscado na matriz: ");
-    scanf("%d", &v);
-
-    int matriz[m][n];
-
-    return matriz;
-}
-
+/* Principal */
 int main(){
-    int m, n, t, v, matriz;
-
+    int linhas, colunas, threads, valor, erro;
     char nome_arq[50];
+    float **matriz;
 
-    printf("\nDigite o nome do arquivo input: ");
-    scanf("%s", nome_arq);
+    printf("\nDigite o nome do arquivo de dados: ");
+    fgets(nome_arq, 50, stdin);
 
     printf("\nDigite a quantidade de Threads a serem utilizadas: ");
-    scanf("%d", &t);
+    scanf("%d", &threads);
 
-    matriz = configuraMatriz(nome_arq, t);
+    printf("\nDigite o numero de linhas da matriz: ");
+    scanf("%d", &linhas);
 
-    FILE *fp = fopen(nome_arq, "r");
-    
-    if (fp == NULL) {
-        printf("Arquivo não encontrado!");
+    printf("\nDigite o numero de colunas da matriz: ");
+    scanf("%d", &colunas);
+
+    printf("\nDigite o valor a ser buscado na matriz: ");
+    scanf("%d", &valor);
+
+    /* Chamando funcao que aloca matriz e verificando se a alocacao
+    foi bem sucedida */
+    matriz = alocarMatriz(linhas, colunas);
+    if(matriz == NULL){
+        printf("\nERRO: nao foi possivel alocar!\n");
     }
-
     
-
-    //for(i = 0; i < EOF; i++);
-
-
-
     return 0;
 }
+
+/* ------------------------------------------------------------------------- */
+
+float **alocarMatriz(int linhas, int colunas){
+
+    float **matriz;
+    /* alocando linhas */
+    matriz = (float **)calloc(linhas, sizeof(float *));
+
+    /* tratamento de erros */
+    if (matriz == NULL) {
+        printf("\nERRO: Memoria insuficiente para alocar a matriz!\n");
+        return NULL;
+    }
+    
+    /* alocando colunas */
+    for(int i = 0; i < linhas; i++){
+        matriz[i] = (float *)calloc(colunas, sizeof(float));
+        if(matriz[i] == NULL){
+            printf("\nERRO: Memoria insuficiente para alocar a matriz!\n");
+            return NULL;
+        }
+    }
+    return matriz;
+}
+/* ------------------------------------------------------------------------- */
+
